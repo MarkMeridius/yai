138eddf0c3208baa7dc3f26dbf7a03ded76613bc
Finn <34718806+FinnDore@users.noreply.github.com>
2022-09-23 22:16:12 +0100

    two basic commands

--- apps/tauri-client/src/main.rs
+++ apps/tauri-client/src/main.rs
@@ -5,7 +5,11 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn generate_image(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+#[tauri::command]
+fn describe_gallary(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
